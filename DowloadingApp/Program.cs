b701089726480363b771ae21044c61a4064b529b
Доллар Музыка <dollarmudic@gmail.com>
2019-05-28 18:32:23 +0600

    Добавьте файлы проекта.

---
+++ DowloadingApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DowloadingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Uri uri = new Uri("https://www.thinkbroadband.com/assets/images/download-files/iconDownload-1GB.png");
+            //string remoteUri = "http://ipv4.download.thinkbroadband.com/";
+            string fileName = "1GB.zip"/*, myStringWebResource*/;
+            //myStringWebResource = remoteUri + fileName;
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://www.thinkbroadband.com/assets/images/download-files/iconDownload-1GB.png", fileName);
+            }
+
+
+            //using (var client = new WebClient())
+            //{
+            //    client.DownloadFile("https://www.thinkbroadband.com/assets/images/download-files/iconDownload-1GB.png", "a.mpeg");
+            //}
+        }
+    }
+}
---
+++ DowloadingApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DowloadingApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DowloadingApp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("46569c92-10c7-45a8-bf78-ab713d95f728")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
